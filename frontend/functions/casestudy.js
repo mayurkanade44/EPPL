1302da35507fcdde1e87846d8208c302be073507
mayurkanade44 <mayurkanade44@gmail.com>
2022-02-19 18:00:08 +0530

    case study table function added

---
+++ frontend/functions/casestudy.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const Airtable = require("airtable-node");
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base("appGtLYDf2nNsBQP5")
+  .table("casestudy");
+
+exports.handler = async (event, context) => {
+  try {
+    const { records } = await airtable.list({ maxRecords: 200 });
+    const services = records.map((service) => {
+      const { id } = service;
+      const { name, descriptions, business, featured_img } = service.fields;
+      const img = featured_img[0].url;
+      return {
+        id,
+        name,
+        descriptions,
+        business,
+        img,
+      };
+    });
+    return {
+      headers: { "Access-Control-Allow-Origin": "*" },
+      statusCode: 200,
+      body: JSON.stringify(services),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "Server error",
+    };
+  }
+};
